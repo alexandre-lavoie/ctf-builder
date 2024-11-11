966f0fe5d459665db0ad3dfe124326442abe4cb8
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2024-11-11 17:17:00 -0500

    Doc generation improvement

--- tests/integration/test_documentation.py
+++ tests/integration/test_documentation.py
@@ -25,3 +25,9 @@ def test() -> None:
 
         with open(os.path.join(temp_dir, "challenge.json")) as h:
             assert json.load(h)
+
+        with open(os.path.join(temp_dir, "teams.json")) as h:
+            assert json.load(h)
+
+        with open(os.path.join(temp_dir, "setup.json")) as h:
+            assert json.load(h)
