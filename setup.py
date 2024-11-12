15754d8a45e4d814f9e0e28d58017882d628e17d
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2024-11-12 14:12:16 -0500

    Bump to 0.0.17

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ package_name = "ctf_builder"
 
 setuptools.setup(
     name=package_name,
-    version="0.0.16",
+    version="0.0.17",
     license="MIT",
     author="Alexandre Lavoie",
     author_email="alexandre.lavoie00@gmail.com",
