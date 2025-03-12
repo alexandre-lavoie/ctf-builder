9addc10f0db78c9eb6632daa464f32b1bb6d9ca6
Alexandre Lavoie <36684879+alexandre-lavoie@users.noreply.github.com>
2025-03-12 19:17:22 -0400

    0.0.21 (#21)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ package_name = "ctf_builder"
 
 setuptools.setup(
     name=package_name,
-    version="0.0.20",
+    version="0.0.21",
     license="MIT",
     author="Alexandre Lavoie",
     author_email="alexandre.lavoie00@gmail.com",
